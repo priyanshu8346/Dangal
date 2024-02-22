b5af4aa8c2be0f82b5b0eefba338601d3ae5d106
Priyanshu Agrawal <priyanshuagrawal419@gmail.com>
2024-02-22 22:41:27 +0530

    final update

---
+++ app.js
@@ -0,0 +1,100 @@
+/* Created by Tivotal */
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+// document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+//   btn.onclick = () => {
+//     let color = btn.style.background;
+//     document.querySelector(":root").style.setProperty("--theme-color", color);
+//   };
+// });
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Unix timestamp (in seconds) to count down to
+  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(twoDaysFromNow)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  // Toggle theme
+  var interval = setInterval(() => {
+    let body = document.body;
+    body.classList.toggle('light-theme');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+  }, 5000);
+  
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
+
+
